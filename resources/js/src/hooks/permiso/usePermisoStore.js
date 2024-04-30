9aee22d513a3e53c13d408f1350fe2b3f485a4c0
CristhianRec <azw1021@gmail.com>
2024-04-30 13:07:56 -0500

    window open pdf

--- resources/js/src/hooks/permiso/usePermisoStore.js
+++ resources/js/src/hooks/permiso/usePermisoStore.js
@@ -30,15 +30,15 @@ export const usePermisoStore = () => {
             const pdfBlob = new Blob([response.data], {
                 type: "application/pdf",
             });
-            const url = window.URL.createObjectURL(pdfBlob);
+            const url = window.open(URL.createObjectURL(pdfBlob));
             //console.log(url);
-            const tempLink = document.createElement("a");
+            /* const tempLink = document.createElement("a");
             tempLink.href = url;
             tempLink.setAttribute("download", "permiso.pdf");
             document.body.appendChild(tempLink);
             tempLink.click();
 
-            document.body.removeChild(tempLink);
+            document.body.removeChild(tempLink); */
             window.URL.revokeObjectURL(url);
             dispatch(onLoading(false));
         } catch (error) {
@@ -58,15 +58,15 @@ export const usePermisoStore = () => {
             const pdfBlob = new Blob([response.data], {
                 type: "application/pdf",
             });
-            const url = window.URL.createObjectURL(pdfBlob);
+            const url = window.open(URL.createObjectURL(pdfBlob));
             //console.log(url);
-            const tempLink = document.createElement("a");
+           /*  const tempLink = document.createElement("a");
             tempLink.href = url;
             tempLink.setAttribute("download", "permiso.pdf");
             document.body.appendChild(tempLink);
             tempLink.click();
 
-            document.body.removeChild(tempLink);
+            document.body.removeChild(tempLink); */
             window.URL.revokeObjectURL(url);
             dispatch(onLoading(false));
             dispatch(onSetActivatePermiso(null));
