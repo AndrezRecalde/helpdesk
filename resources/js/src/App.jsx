60086f459cedafb7ece40a3f15ead4c3b80106ab
CristhianRec <azw1021@gmail.com>
2024-11-06 09:04:39 -0500

    styles !important

--- resources/js/src/App.jsx
+++ resources/js/src/App.jsx
@@ -4,13 +4,13 @@ import "@mantine/dates/styles.css";
 import "@mantine/spotlight/styles.css";
 import "@mantine/tiptap/styles.css";
 import "mantine-react-table/styles.css";
+import { Suspense } from "react";
 import { MantineProvider } from "@mantine/core";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./routes/router/AppRouter";
 import { theme } from "./theme";
 import { store } from "./store";
-import { Suspense } from "react";
 //import { AppRouter2 } from "./routes/AppRouter2";
 
 export const App = () => {
