f6ddc4e2c88e29a2930ac00cd42f115023285841
AndrezRecalde <azw1021@gmail.com>
2025-09-01 01:42:31 -0500

    array imagenes

--- resources/js/src/pages/auth/AuthPage.jsx
+++ resources/js/src/pages/auth/AuthPage.jsx
@@ -4,14 +4,14 @@ import { useTitlePage } from "../../hooks";
 import classes from "../../assets/styles/modules/auth/AuthPageBackground.module.css";
 
 const AuthPage = () => {
-    const imagenes = JSON.parse(localStorage.getItem("service_images"));
+    const imagenes = JSON.parse(localStorage.getItem("service_images")) || [];
     useTitlePage("Helpdesk | Login");
 
     return (
         <div
             className={classes.wrapper}
             style={{
-                backgroundImage: `url(${imagenes[0]?.imagen_fondo})`,
+                backgroundImage: `url(${imagenes[0]?.imagen_fondo || 'https://placehold.co/1920x1080'})`,
                 backgroundSize: "cover",
                 backgroundPosition: "center",
             }}
@@ -25,7 +25,7 @@ const AuthPage = () => {
                 <Image
                     radius="xs"
                     h={180}
-                    src={imagenes[0]?.imagen_login}
+                    src={imagenes[0]?.imagen_login || 'https://placehold.co/600x400?text=Placeholder'}
                     fallbackSrc="https://placehold.co/600x400?text=Placeholder"
                 />
                 <AuthForm />
