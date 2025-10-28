db48fc0f37eecc436233c8a52735ecaa4b60ec29
AndrezRecalde <azw1021@gmail.com>
2025-10-27 19:24:20 -0500

    se ajusta el grid

--- resources/js/src/components/home/section/ServiceSection.jsx
+++ resources/js/src/components/home/section/ServiceSection.jsx
@@ -10,7 +10,7 @@ import { MenuItems } from "../../../layouts";
 
 export const ServiceSection = ({ title, menu, classes }) => {
     const theme = useMantineTheme();
-    const gridProps = { cols: { base: 2, sm: 2, md: 5, lg: 5 }, mt: "md" };
+    const gridProps = { cols: { base: 1, sm: 2, md: 5, lg: 5 }, mt: "md" };
 
     return (
         <Card withBorder radius="md" className={classes.card} mb={20}>
